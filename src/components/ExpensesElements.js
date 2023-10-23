1c8e88dd760d26abe79d5f75c091be45e460128c
Jakub <havier1993@gmail.com>
2023-10-23 09:47:40 +0200

    finish styling

--- src/components/ExpensesElements.js
+++ src/components/ExpensesElements.js
@@ -5,8 +5,10 @@ const ExpensesElements = () => {
 			<div className='expenses-elements__date'>
 				<div>2019</div>
 			</div>
-			<h1>Title</h1>
-			<div className='expeneses-elements__price'>400$</div>
+			<div className='expenses-elements__items'>
+				<h1>Title</h1>
+				<div className='expeneses-elements__price'>400$</div>
+			</div>
 		</div>
 	)
 }
