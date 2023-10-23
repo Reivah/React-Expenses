02c391a6a441fcc79873c7646847e1385acc81dc
Jakub <havier1993@gmail.com>
2023-10-23 08:39:06 +0200

    added expense item

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ExpensesElements from "./components/ExpensesElements";
+
 function App() {
   return (
     <div>
       <h2>Expenses List</h2>
+      <ExpensesElements></ExpensesElements>
     </div>
   );
 }
---
+++ src/components/ExpensesElements.js
@@ -0,0 +1,13 @@
+const ExpensesElements = () => {
+	return (
+		<div className='expenses-elements'>
+			<div className='expenses-elements__date'>
+				<div>2019</div>
+			</div>
+			<h1>Title</h1>
+			<div className='expeneses-elements__price'>400$</div>
+		</div>
+	)
+}
+
+export default ExpensesElements
