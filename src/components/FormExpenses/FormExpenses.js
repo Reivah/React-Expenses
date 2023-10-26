6f65eb81bb9f6caeb3a496340fe75105f606d72a
Jakub <havier1993@gmail.com>
2023-10-26 20:24:30 +0200

    added form box

---
+++ src/components/FormExpenses/FormExpenses.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FormExpenses = () => {
+    return (
+        <form>
+            <div className='from-main'>
+                <div className='form-main__box'>
+                   <label>title</label>
+                   <input/>
+                </div>
+            </div>
+        </form>
+    )
+}
