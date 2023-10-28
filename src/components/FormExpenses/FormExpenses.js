a6342c5058e479aeed4287cd207552595be94501
Jakub <havier1993@gmail.com>
2023-10-28 15:30:26 +0200

    added title handle

--- src/components/FormExpenses/FormExpenses.js
+++ src/components/FormExpenses/FormExpenses.js
@@ -7,16 +7,26 @@ const [expenseName, setExpenseName] = useState('')
 const [price, setPrice] = useState('')
 const [date, setDate] = useState('')
 
-const setExpenseHandler = event => {
-    setExpenseName(event.target.value)
-}
+// const setExpenseHandler = event => {
+//     setExpenseName(event.target.value)
+// }
 
-const setPriceHandler = event => {
-    setPrice(event.target.value)
-}
+// const setPriceHandler = event => {
+//     setPrice(event.target.value)
+// }
 
-const setDateHandler = event => {
-    setDate(event.target.value)
+// const setDateHandler = event => {
+//     setDate(event.target.value)
+// }
+
+const inputHandlers = (input, value) => {
+    if(input === 'Expense name'){
+        setExpenseName(value)
+    }else if(input === 'Price'){
+        setPrice(value)
+    }else {
+        setDate(value)
+    }
 }
 
     return (
@@ -24,15 +34,15 @@ const setDateHandler = event => {
             <div className='form-main'>
                 <div className='form-main__box'>
                    <label type='text'>Expense name</label>
-                   <input type='text' placeholder='Description' onChange={setExpenseHandler}/>
+                   <input type='text' placeholder='Description' onChange={(event) => {inputHandlers('Expense name', event.target.value)}}  />
                 </div>
                 <div className='form-main__box'>
                    <label type='number'>Price</label>
-                   <input type='number' placeholder='Value' onChange={setPriceHandler}/>
+                   <input type='number' placeholder='Value'  />
                 </div>
                 <div className='form-main__box'>
                    <label type='date'>Date</label>
-                   <input type='date' onChange={setDateHandler}/>
+                   <input type='date' />
                 </div>
             </div>
             <div className='button-box'>
