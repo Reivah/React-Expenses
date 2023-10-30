8f031231ca097f7ef833a4f30fa86d55f1c2e715
Jakub <havier1993@gmail.com>
2023-10-30 17:34:40 +0100

    providing data from children to parent component

--- src/components/FormExpenses/FormBox.js
+++ src/components/FormExpenses/FormBox.js
@@ -1,6 +1,11 @@
 import './FormBox.css'
 import FormExpenses from './FormExpenses'
 const FormBox = () => {
+
+    const expensesData = (expenses) => {
+        
+    }
+
     return (
         <div className='form-box__main'>
             <FormExpenses/>
