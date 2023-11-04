6629bd7108ca6214e678845aaa0014d38275ac99
Jakub <havier1993@gmail.com>
2023-11-04 15:57:35 +0100

    updating app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
+import { useState } from 'react'
 import './App.css'
 import ExpenseItems from './components/ExpenseItems';
 import FormBox from './components/FormExpenses/FormBox';
 
+const expenseArray = [
+  {
+    title: 'Gaming computer',
+    price: 5999.50,
+    date: new Date(2022,5,20)
+  },
+  {
+    title: 'OLED TV',
+    price: 2500.90,
+    date: new Date(2022,7,10)
+  },
+  {
+    title: 'Food',
+    price: 80.99,
+    date: new Date(2023,1,15)
+  },
+  {
+    title: 'Books',
+    price: 230.99,
+    date: new Date(2023,2,10)
+  }]
+
 function App() {
-  const expenseArray = [
-    {
-      title: 'Gaming computer',
-      price: 5999.50,
-      date: new Date(2022,5,20)
-    },
-    {
-      title: 'OLED TV',
-      price: 2500.90,
-      date: new Date(2022,7,10)
-    },
-    {
-      title: 'Food',
-      price: 80.99,
-      date: new Date(2023,1,15)
-    },
-    {
-      title: 'Books',
-      price: 230.99,
-      date: new Date(2023,2,10)
-    }]
+  
 
     const dataExpenseHandler = (expenses) => {
         console.log(expenses);
