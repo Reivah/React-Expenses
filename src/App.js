19cab9f5a9c401fc00b163b6970a5a882d92c83f
Jakub <havier1993@gmail.com>
2023-11-07 10:24:08 +0100

    updating expenses year

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
 import { useState } from 'react'
 import './App.css'
-import ExpenseItems from './components/ExpenseItems';
-import FormBox from './components/FormExpenses/FormBox';
+import ExpenseItems from './components/ExpenseItems'
+import FormBox from './components/FormExpenses/FormBox'
 
 const expenseArray = [
-  {
-    id: '1',
-    title: 'Gaming computer',
-    price: 5999.50,
-    date: new Date(2022,5,20)
-  },
-  {
-    id: '2',
-    title: 'OLED TV',
-    price: 2500.90,
-    date: new Date(2022,7,10)
-  },
-  {
-    id: '3',
-    title: 'Food',
-    price: 80.99,
-    date: new Date(2023,1,15)
-  },
-  {
-    id: 4,
-    title: 'Books',
-    price: 230.99,
-    date: new Date(2023,2,10)
-  }]
+	{
+		id: '1',
+		title: 'Gaming computer',
+		price: 5999.5,
+		date: new Date(2022, 5, 20),
+	},
+	{
+		id: '2',
+		title: 'OLED TV',
+		price: 2500.9,
+		date: new Date(2022, 7, 10),
+	},
+	{
+		id: '3',
+		title: 'Food',
+		price: 80.99,
+		date: new Date(2023, 1, 15),
+	},
+	{
+		id: 4,
+		title: 'Books',
+		price: 230.99,
+		date: new Date(2023, 2, 10),
+	},
+]
 
 function App() {
+	const [expense, setExpenses] = useState(expenseArray)
 
-  const [expense, setExpenses] = useState(expenseArray)
+	const dataExpenseHandler = expenses => {
+		setExpenses(prevExpense => {
+			return [expenses, ...prevExpense]
+		})
+	}
 
-    const dataExpenseHandler = (expenses) => {
-        setExpenses((prevExpense) => {
-          return ([expenses, ...prevExpense])
-        })
-    }
-
-  return (
-    <div>
-      <h2 className='expenses-title'>Expenses List</h2>
-      <div className='app-main'>
-      <FormBox onDataExpenseHandler={dataExpenseHandler}/>
-      <ExpenseItems expensesArray={expense}></ExpenseItems>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<h2 className='expenses-title'>Expenses List</h2>
+			<div className='app-main'>
+				<FormBox onDataExpenseHandler={dataExpenseHandler} />
+				<ExpenseItems expensesArray={expense}></ExpenseItems>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -20,29 +20,7 @@ const ExpenseItems = props => {
 	</div>
 
     )
-    // return (
-	// <div>
-	// 	<ExpenseYearSearch onSelectedYear={selectedYear} onSelectedYearValue={selectedYearValue}/>
-	// 	<ExpensesElements
-	// 		title={props.array[0].title}
-	// 		price={props.array[0].price}
-	// 		date={props.array[0].date}/>
-	// 	<ExpensesElements
-	// 		title={props.array[1].title}
-	// 		price={props.array[1].price}
-	// 		date={props.array[1].date}/>
-	// 	<ExpensesElements
-	// 		title={props.array[2].title}
-	// 		price={props.array[2].price}
-	// 		date={props.array[2].date}/>
-	// 	<ExpensesElements
-	// 		title={props.array[3].title}
-	// 		price={props.array[3].price}
-	// 		date={props.array[3].date}
-	// 	/>
-	// </div>
 
-    // )
 }
 
 
