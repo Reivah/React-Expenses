4f0a693e9204d1d60cd4e5f6533cca4a7001ea46
Jakub <havier1993@gmail.com>
2023-11-07 10:32:02 +0100

    created expenseFilter const

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -2,26 +2,23 @@ import { useState } from 'react'
 import ExpensesElements from './ExpensesElements'
 import ExpenseYearSearch from './ExpenseYearSearch'
 const ExpenseItems = props => {
-
 	const [selectedYear, setSelectedYear] = useState('2023')
 
-	const selectedYearValue = (yearValue) => {
-		const year = yearValue 
+	const selectedYearValue = yearValue => {
+		const year = yearValue
 		setSelectedYear(year)
-		
 	}
 
-    return (
-	<div>
-		<ExpenseYearSearch onSelectedYear={selectedYear} onSelectedYearValue={selectedYearValue}/>
-		{props.expensesArray.map(expense => (
-			<ExpensesElements key={expense.id} title={expense.title} price={expense.price} date={expense.date} />
-		))}
-	</div>
+	const expenseFilter = props.expensesArray.filter(expense => expense.date.getFullYear().toString() === selectedYear)
 
-    )
-
+	return (
+		<div>
+			<ExpenseYearSearch onSelectedYear={selectedYear} onSelectedYearValue={selectedYearValue} />
+			{props.expensesArray.map(expense => (
+				<ExpensesElements key={expense.id} title={expense.title} price={expense.price} date={expense.date} />
+			))}
+		</div>
+	)
 }
-
 
 export default ExpenseItems
